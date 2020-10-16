772e34748ea6b7e18e15701d4562167ab884465c
Pakatip Kasai <honey.shibuyaa@gmail.com>
2020-10-16 19:06:59 +0900

    Add files via upload

---
+++ 2_Python_OS/guests_check.py
@@ -0,0 +1,34 @@
+#this this how to modify file (read/write/append/delete)
+initial_guests = ["Atsushi","Bee","Shingo","Taiki","Daiki","Hana"]
+
+#check if guests.txt file have right names written
+def check_guests():
+    with open("guests.txt","r") as file:
+        for name in file:
+            print(name)
+
+#creating empty text file and write names in
+file = open("guests.txt","w")
+for name in initial_guests:
+    file.write(name + "\n")
+check_guests()
+
+#adding new guests
+new_guests = ["Joy","Toru","Tomo"]
+with open("guests.txt","a") as file:
+    for name in new_guests:
+        file.write(name + "\n")
+check_guests()
+
+#removing the guests that have checked out
+checkout_guests = ["Shingo","Taiki"]
+temp_list = []
+
+with open("guests.txt","r") as file:
+    for name in file:
+        temp_list.append(name.strip())
+with open("guests.txt","w") as file:
+    for name in temp_list:
+        if name not in checkout_guests:
+            file.write(name + "\n")
+check_guests()
